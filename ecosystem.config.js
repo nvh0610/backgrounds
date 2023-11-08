1e1f7e2b2e930543c8a19cad2a5a7e5284af9f75
Pham Minh Tuan <phmtuan313@gmail.com>
2023-11-08 17:17:37 +0700

    Add pm2 file

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apps: [
+        {
+            name: 'backgrounds',
+            script: 'node',
+            args: ['node_modules/.bin/strapi', 'start'],
+        },
+    ],
+};
